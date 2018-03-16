f6bf2f116c0c5b4204f23dfddc0726c0d0a803ba
sureshsuthib <36033853+sureshsuthib@users.noreply.github.com>
2018-03-16 11:16:03 +0000

    Rename prime.c to numprime.c

---
+++ numprime.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int m, n, i, flag;
+    scanf("%d %d", &m, &n);
+  for(m=m+1;m<n;m++)
+    {
+        flag = 0;
+
+        for(i = 2; i <= m/2; ++i)
+        {
+            if(m % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("%d ", m);
+    }
+
+    return 0;
+}
